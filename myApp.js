fadb834457641f3e2eebf733894762e5af3a0c47
gwin <gwin@kali>
2025-01-23 12:10:49 +0100

    worked on getting the schema to work

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 const personSchema = new mongoose.Schema({
   name: String,
   age: Number,
-  favoriteFood: String,
+  favoriteFoods: String,
 })
 
 
