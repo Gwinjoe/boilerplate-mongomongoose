b532a4c8997219bef0104f0b528848836bc21b72
gwin <gwin@kali>
2025-01-23 12:07:22 +0100

    create a model

--- myApp.js
+++ myApp.js
@@ -4,7 +4,14 @@ const mongoose = require("mongoose")
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFood: String,
+})
+
+
+let Person = mongoose.model('Person', personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
