bc8af5334e1ecca50b787d5933122ceafe86b558
gwinjoe <godfreyjn873@gmail.com>
2025-03-28 15:24:54 +0100

    uniquefalse

--- myApp.js
+++ myApp.js
@@ -9,7 +9,6 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 const personSchema = new mongoose.Schema({
   name: {
-    unique: true,
     type: String,
   },
   age: Number,
